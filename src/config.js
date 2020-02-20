cdd907d26504a91b3af9455ffac207699ac8bad8
Timothy Meyer <dgnt22@gmail.com>
2020-02-20 16:40:05 +1300

    Navbar, basic routing, start AWS Amplify integration

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "dgnt22.notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://oj80uekfo0.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Z4ZkgUNMd",
+    APP_CLIENT_ID: "2gh79h9fbasglj83jfvrmhnd4n",
+    IDENTITY_POOL_ID: "us-east-1:f6ff538e-4b76-48ef-818d-6b70363df6cd"
+  }
+};
